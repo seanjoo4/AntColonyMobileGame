7be2f918af9b2db858738207d42ffd35b0649773
seanjoo <joo21@purdue.edu>
2020-06-07 14:11:21 -0400

    general updates in PopUpgrade class

--- app/src/main/java/com/e/antcolony/PopUpgrade.java
+++ app/src/main/java/com/e/antcolony/PopUpgrade.java
@@ -9,17 +9,21 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class PopUpgrade extends AppCompatActivity {
+    // just for implementing for now (can/probably should, change)
+    int upgrade_tier = 5;
+
     /**
      * Initializes the activity.
      *
      * @param savedInstanceState used when activity needs to be created/recreated
      */
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pop_upgrade);
+
         Intent intent = getIntent();
+
         final int unemployedCount = intent.getIntExtra("unemployed", 0);
         final int victoryCount = intent.getIntExtra("victoryCount", 0);
         final int lossCount = intent.getIntExtra("lossCount", 0);
@@ -28,16 +32,17 @@ public class PopUpgrade extends AppCompatActivity {
         final int growPressed = intent.getIntExtra("growPressed", 0);
 
         TextView textViewIdleAnts = findViewById(R.id.idle_ants);
-        textViewIdleAnts.setText("Idle Ants: " + unemployedCount);
         TextView textViewVictory = findViewById(R.id.biteVictory);
-        textViewVictory.setText("Territories Owned: " + victoryCount);
         TextView textViewLoss = findViewById(R.id.biteLoss);
-        textViewLoss.setText("Bite Defeats: " + lossCount);
         TextView textViewSuccessful = findViewById(R.id.liftSuccessful);
-        textViewSuccessful.setText("Successful Lifts: " + successfulLift);
         TextView textViewUnsuccessful = findViewById(R.id.liftUnsuccessful);
-        textViewUnsuccessful.setText("Unsuccessful Lifts: " + unsuccessfulLift);
         TextView textViewGrowPressed = findViewById(R.id.growPressed);
+
+        textViewIdleAnts.setText("Idle Ants: " + unemployedCount);
+        textViewVictory.setText("Territories Owned: " + victoryCount);
+        textViewLoss.setText("Bite Defeats: " + lossCount);
+        textViewSuccessful.setText("Successful Lifts: " + successfulLift);
+        textViewUnsuccessful.setText("Unsuccessful Lifts: " + unsuccessfulLift);
         textViewGrowPressed.setText("Grow Pressed: " + growPressed);
 
         // upgrade button
@@ -51,7 +56,10 @@ public class PopUpgrade extends AppCompatActivity {
              */
             @Override
             public void onClick(View v) {
-
+                if (victoryCount > upgrade_tier) {
+                    // do something when upgrading tier (ex: dynamic background in MainActivity)
+                    upgrade_tier += 5;
+                }
             }
         });
 
